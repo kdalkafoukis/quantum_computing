cb9b5e0c750040edd22110d9e17211bb38accfd2
kdalkafoukis <kdalkafoukis@yahoo.gr>
2020-09-04 22:53:58 +0100

    merging in one file - more functions - cleaning

--- generate_mixed_state.py
+++ generate_mixed_state.py
@@ -1,7 +1,11 @@
 import numpy as np
-from scipy.linalg import orth, polar, dft, schur
-from scipy import linalg
-from utils import allCombinations, testIfArrayIsUnitary, gram_schmidt
+from scipy.linalg import dft
+from pyquil import get_qc, Program
+from pyquil.quil import DefGate
+from pyquil.gates import *
+from pyquil.api import local_forest_runtime, WavefunctionSimulator
+import numpy as np
+import sys
 
 def getNumberOfElements(state):
     numberOfElements = 0
@@ -17,15 +21,6 @@ def getFlatDft(numberOfElements):
         for j in range(0,numberOfElements):
             flatDft.append(dftArr[i][j])
     return flatDft
-
-# def getFlatDft(numberOfElements):
-#     numberOfElements = numberOfElements + 1
-#     dftArr = dft(numberOfElements)
-#     flatDft = []
-#     for i in range(1,numberOfElements):
-#         for j in range(1,numberOfElements):
-#             flatDft.append(dftArr[i][j])
-#     return flatDft
 
 def getDensityDft(density_matrix, flatDft, rows):
     counter = 0
@@ -61,16 +56,52 @@ def generate_density_matrix(state, rows):
     density_matrix = state.transpose().dot(state)
     numberOfElements = getNumberOfElements(state)
     flatDft = getFlatDft(numberOfElements)
-    # print(flatDft)
 
     density_matrix = getDensityDft(density_matrix, flatDft, rows)
     density_matrix = applyOnesToDensityMatrix(density_matrix,numberOfElements, rows)
-
     density_matrix = 1 / np.sqrt(numberOfElements) * density_matrix
-    # print(density_matrix)
-    # print(testIfArrayIsUnitary(density_matrix))
+
     return density_matrix
 
+def generateState(arr):
+    state = np.array([[
+        0, 1, 1, 1, 
+        0, 1, 1, 1, 
+    ]], dtype=complex)
+    return state
 
+def shiftState(state, rows, prog):
+    bits = shiftedState(state, rows)
+
+    for position, bit in enumerate(reversed(bits)):
+        if(bool(bit)):
+            prog += Program(X(position))
+
+def applyDensityMatrix(state, rows, prog):
+    arr = generate_density_matrix(state, rows)
+
+    definition = DefGate("operator", arr)
+    prog += Program(definition)
+    operator = definition.get_constructor()
+
+    num_of_qubits = int(np.log2(rows))
+    qubits = range(num_of_qubits)
+    qbits = [qubit for qubit in reversed(qubits)]
+    prog += Program(operator(*qbits))
+
+def generateMixedState():
+    prog = Program()
+
+    state = generateState([])
+    rows = len(state[0])
+    shiftState(state, rows, prog)
+    applyDensityMatrix(state, rows, prog)
+
+    wfn = WavefunctionSimulator().wavefunction(prog)
+    prob = wfn.get_outcome_probs()
+
+    print(wfn)
+    # print(prob)
+    
 if __name__ == "__main__":
-    generate_density_matrix()
+    generateMixedState()
