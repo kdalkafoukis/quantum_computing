65f37c98964a63046fd59501a7159d331750afe8
kdalkafoukis <kdalkafoukis@yahoo.gr>
2021-04-16 18:59:05 +0100

    get input - use input to control qdft

--- qdft.py
+++ qdft.py
@@ -6,20 +6,48 @@ from pyquil import Program
 from pyquil.gates import *
 from pyquil.api import WavefunctionSimulator
 import numpy as np
+import sys
 
-def qdft(num_of_iterations = 2):
+
+def applyXgate(num):
     prog = Program()
-    for i in range(num_of_iterations):
+    counter = 0
+    for i in format(num, 'b'):
+        if i == '1':
+            prog += Program(X(int(counter)))
+        counter += 1
+    return prog
+
+
+def getInput(argv):
+    number = 0
+    qubits = 2
+    if (len(argv) == 2 and 2 ** int(argv[0]) > int(argv[1])):
+        try:
+            qubits = int(argv[0])
+            number = int(argv[1])
+            print('input')
+            print('qubits:', qubits)
+            print('number:', number)
+        except ValueError:
+            pass
+    return number, qubits
+
+
+def qdft(qubits=2, num=1):
+    prog = Program()
+    prog += applyXgate(num)
+    for i in range(qubits):
         prog += Program(H(i))
         counter = 2
-        for j in range(i + 1, num_of_iterations):
-            cphase = CPHASE(2 * np.pi / 2 ** counter, i ,j)
+        for j in range(i + 1, qubits):
+            cphase = CPHASE(2 * np.pi / 2 ** counter, i, j)
             prog += Program(cphase)
             counter = counter + 1
     return prog
 
 
-def qdft_in_list_of_qubits(qubits = [0,1,2], qubit_neigboors = [[1,2],[0]]):
+def qdft_in_list_of_qubits(qubits=[0, 1, 2], qubit_neigboors=[[1, 2], [0]]):
     prog = Program()
     for qubit in qubits:
         prog += Program(I(qubit))
@@ -28,13 +56,15 @@ def qdft_in_list_of_qubits(qubits = [0,1,2], qubit_neigboors = [[1,2],[0]]):
         counter = 2
         if(qubit != qubits[len(qubits) - 1]):
             for neighboor in qubit_neigboors[qubit]:
-                cphase = CPHASE(2 * np.pi / 2 ** counter, qubit ,neighboor)
+                cphase = CPHASE(2 * np.pi / 2 ** counter, qubit, neighboor)
                 prog += Program(cphase)
                 counter = counter + 1
     return prog
 
-def main():
-    prog = qdft()
+
+def main(argv):
+    number, qubits = getInput(argv)
+    prog = qdft(qubits, number)
     wfn = WavefunctionSimulator().wavefunction(prog)
     prob = wfn.get_outcome_probs()
     print(wfn)
@@ -42,4 +72,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
